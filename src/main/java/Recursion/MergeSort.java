d4b998d3995929661ad80e5c855e68beb0103ecc
magnuen2k <maen030@student.kristiania.no>
2020-09-19 01:20:58 +0200

    remove main from sortclass

--- src/main/java/Recursion/MergeSort.java
+++ src/main/java/Recursion/MergeSort.java
@@ -2,16 +2,6 @@ package Recursion;
 
 public class MergeSort {
 
-    /*public static void main(String[] args) {
-        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
-
-        mergeSort(intArray, 0, intArray.length);
-
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
-    }*/
-
     public static void mergeSort(int[] input, int start, int end) {
 
         if (end - start < 2) {
